765c311e35925d74f896fa0462bf2652ce58fc9d
Lorenzo <lorenzo.porretti@outlook.fr>
2023-02-08 10:23:01 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Dofus</title>
+</head>
+<body>
+    <header>
+        <section class="backtop">
+            <div class="top">
+                <div class="vide"></div>
+                <div class="englobe">
+                    <div class="rondelle">
+                        <a href="https://www.dofus.com/en/prehome"><img class="logodofus" src="LOGODOFUUUS" alt="Logo Dofus"></a>
+                    </div>
+                </div> 
+                <div class="cre_con">
+                    <ul class="login">
+                        <li class="connect"><a href="login.php">Se connecter</a></li>
+                        <li class="connect"><a href="createlogin.php">Crée un Compte</a></li>
+                    </ul>
+                </div>  
+            </div>
+                
+            <nav>
+                <ul class="nav">
+                    <li><a class="navigateur" href="#index.php">Accueil</a></li>
+                    <li class="presentation">Présentation
+                        <ul class="sousmenu">
+                            <li><a href="classes.php">Classe</a></li>
+                            <li><a href="combat.php">Combat</a></li>
+                            <li><a href="metiers.php">Métier</a></li>
+                            <li><a href="economie.php">Économie</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="navigateur" href="stats.php">Stats</a></li>
+                    <li><a class="navigateur" href="contact.php">Contact</a></li>
+                </ul>
+            </nav>
+        </section>
+    </header>
+
+    <section class="douze">
+        <div class="milieutout">
+                 <div class="map">
+                    <img class="mapun" src="Capture.PNG" alt="">
+                </div>
+            <div class="milieucentre">
+                <div class="pretext">
+                    <p class="textcarrer">Dans ce RPG en ligne, vous pouvez vendre et acheter des ressources grâce à la monnaie du jeu, les "kamas" et exercer de multiples métiers (paysan, chasseur, boulanger, forgeron...). Donjons, chasses aux trésors, monstres, boss, familiers, compagnons et mode joueur contre joueur (JcJ / PvP) vous attendent dans ce jeu stratégie en ligne</p>
+                </div>
+                <div class="pretext">
+                    <p class="textcarrer">Dans ce RPG en ligne, vous pouvez vendre et acheter des ressources grâce à la monnaie du jeu, les "kamas" et exercer de multiples métiers (paysan, chasseur, boulanger, forgeron...). Donjons, chasses aux trésors, monstres, boss, familiers, compagnons et mode joueur contre joueur (JcJ / PvP) vous attendent dans ce jeu stratégie en ligne</p>
+                </div>
+            </div>
+        </div>
+
+    <footer>
+        <ul class="footer">
+            <li class="li_footer"><a class="navigateur" href="https://www.dofus.com/en/prehome">Site officiel</a></li>
+            <li class="li_footer"><a class="navigateur" href="https://twitter.com/DOFUS_EN">Twitter</a></li>
+            <li class="li_footer"><a class="navigateur" href="https://discord.com/invite/0RDH0dqUoTRkCjSF">Discord</a></li>
+            <li class="li_footer"><a class="navigateur" href="Nous_Contacter">Nous Contacter</a></li>
+        </ul>
+    </footer>
+
+    </section>
+</body>
+</html>
